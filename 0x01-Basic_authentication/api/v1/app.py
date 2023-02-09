876447075b8036826d9753a594862659078251b9
michaelgobz <raniahgobz@outlook.com>
2023-02-09 15:23:20 +0000

    updated the excluded paths

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -60,7 +60,7 @@ def before_request() -> str:
     """
     if auth is None:
         return
-    excluded_paths = ['/', '/api/v1/status/',
+    excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
