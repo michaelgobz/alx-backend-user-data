8e9e0ce82e5cba624c70d06c37a1d39aafb94031
michaelgobz <raniahgobz@outlook.com>
2023-02-11 17:35:13 +0000

    added the old routes

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,12 +1,46 @@
 #!/usr/bin/env python3
-""" sessions
+""" Module of Index views
 """
-from flask import Blueprint
+from typing import Any
 
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-from api.v1.views.index import *
-from api.v1.views.users import *
-from api.v1.views.session_auth import *
 
-User.load_from_file()
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> None:
+    """
+    route to test the authorization
+    Returns: str
+    """
+    abort(401, ...)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> None:
+    """
+    for bidden handler
+    Returns: None
+    """
+    abort(403)
